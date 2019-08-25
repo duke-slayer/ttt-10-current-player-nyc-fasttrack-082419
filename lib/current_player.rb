627bd1d1b78f7afd3d6bb498855f89be20291423
Amy Duke <amyyyduke@gmail.com>
2019-08-25 16:04:35 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,10 @@
+#def turn_count(borad)
+  arr = []
+  board.each do |position|
+   # if position == "X" || position == "O"
+     # arr << 1 #shovel method 
+  #  end
+#  end
+ # return arr.length 
+end
+
